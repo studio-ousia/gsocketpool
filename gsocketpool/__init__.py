c1a3b3a667bea975e15fa6e97d68379724125eaf
Ikuya Yamada <ikuya@ikuya.net>
2014-03-01 23:53:01 +0900

    add exception modules to the package root

--- gsocketpool/__init__.py
+++ gsocketpool/__init__.py
@@ -3,3 +3,4 @@
 from connection import Connection
 from connection import TcpConnection
 from pool import Pool
+from exceptions import ConnectionNotFoundError, PoolExhaustedError
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsocketpool',
-    version='0.1.1',
+    version='0.1.2',
     description='A simple connection pool for gevent',
     author='Studio Ousia',
     author_email='admin@ousia.jp',
