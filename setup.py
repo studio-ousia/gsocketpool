e196720062f58709650362d617465f7a5d3e15ad
Ikuya Yamada <ikuya@ikuya.net>
2015-10-12 16:26:50 +0900

    bump version

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsocketpool',
-    version='0.1.4',
+    version='0.1.5',
     description='A simple connection pool for gevent',
     author='Studio Ousia',
-    author_email='admin@ousia.jp',
+    author_email='ikuya@ousia.jp',
     url='http://github.com/studio-ousia/gsocketpool',
     packages=find_packages(),
     license=open('LICENSE').read(),
@@ -20,6 +20,9 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ),
     install_requires=[
         'gevent',
